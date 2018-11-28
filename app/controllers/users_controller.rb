fbc82ebe5b83054d412fd6db3303f08a1cf7ff58
James McCann <jamesredux@gmail.com>
2018-11-28 20:23:45 +0700

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
+      flash[:success] =  "Welcome to Private Events"
   		redirect_to root_url
   	else
   		render 'new'
