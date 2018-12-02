6b1c11feac1f1bbf336176769328a3d1e03ea6fb
James McCann <jamesredux@gmail.com>
2018-12-02 19:44:34 +0700

    add past and future methods to Event class

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+def upcoming_events(user)
+	@attended_events = @user.attended_events
+	upcoming_events = @attended_events.upcoming.paginate(page: params[:page], per_page: 5)
+end
+
+def past_events(user)
+	@attended_events = @user.attended_events
+	past_events = @attended_events.past.paginate(page: params[:page], per_page: 5)
+	
+end
+
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,10 @@ class Event < ApplicationRecord
 	belongs_to :creator, :class_name => "User"
 	has_many :event_attendees, :foreign_key => "attended_event_id"
 	has_many :attendees, :through => :event_attendees, :source => "attendee"
-	
+
+	#scopes
+	scope :upcoming, -> { where('date >= :now', {now: Time.now }) }
+	scope :past, -> { where('date < :now', {now: Time.now}) } 
 	
 	
 
