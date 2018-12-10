8b8a5d76ed055147fcd291bfd8bcc1dbf570a4e4
James McCann <jamesredux@gmail.com>
2018-12-10 16:14:10 +0700

    update associations order

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,9 @@ class Event < ApplicationRecord
 
 
 	belongs_to :creator, :class_name => "User"
-	has_many :attendees, :through => :event_attendees, :source => "attendee"
 	has_many :event_attendees, :foreign_key => "attended_event_id", dependent: :destroy
+	has_many :attendees, :through => :event_attendees, :source => "attendee"
+
 
 	#scopes
 	scope :upcoming, -> { where('date >= :now', {now: Time.now }) }
