95a3830d103f8ad76564e5eb6a62e3de9ce25da6
James McCann <jamesredux@gmail.com>
2018-12-10 16:05:17 +0700

    amend migration

--- db/migrate/20181201084457_add_creator_ref_to_events.rb
+++ db/migrate/20181201084457_add_creator_ref_to_events.rb
@@ -1,5 +1,6 @@
 class AddCreatorRefToEvents < ActiveRecord::Migration[5.1]
   def change
-    add_reference :events, :creator, foreign_key: true
+    add_column :events, :creator_id, :integer 
+    
   end
 end
